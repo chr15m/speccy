c2d9fe5d03edb85915557703a5f4b0dd535e74aa
Chris McCormick <chris@mccormick.cx>
2017-05-12 16:36:53 +0800

    Paste sfxr json def to get edn.

--- src/speccy/core.cljs
+++ src/speccy/core.cljs
@@ -3,12 +3,29 @@
             [speccy.engine :refer [looper scheduler player catch-background-tab]]
             [speccy.scratch]))
 
+(defn remove-defaults [m]
+  m)
+
+(defn json-to-edn [v]
+  (-> v
+      (js/JSON.parse)
+      (remove-defaults)
+      (js->clj :keywordize-keys true)
+      (str)
+      (clojure.string/replace "," "\n")))
+
 ;; -------------------------
 ;; Views
 
 (defn home-page []
-  [:div
-   [:img {:src "img/speccy.png"}]])
+  (let [result (reagent/atom "")]
+    (fn []
+      [:div
+       [:img {:src "img/speccy.png"}]
+       [:textarea {:rows 36
+                   :placeholder "paste json sfxr def here"
+                   :on-change #(reset! result (json-to-edn (-> % .-target .-value)))}]
+       [:pre @result]])))
 
 ;; -------------------------
 ;; Initialize app
